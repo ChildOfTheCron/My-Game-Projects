024ac14e4eb432471f0e57f472f1352c6d4af804
Hobomechanistic <dejagerrob@gmail.com>
2014-11-24 23:21:56 +0000

    Deleted dupes
    Removed duplicate code

--- EnemyDroneMarker.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using System.Collections;
-
-public class EnemyDroneMarker : MonoBehaviour {
-
-    public void OnDrawGizmos() {
-		Gizmos.DrawIcon(transform.position, "EnemyDroneMarker.gif");
-    }
-}
